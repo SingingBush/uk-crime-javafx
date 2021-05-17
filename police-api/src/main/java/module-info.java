20bb8d00d799f287d9d35e8757eb5ea4280799a8
singingbush <singingbush@hotmail.com>
2021-05-17 21:55:22 +0100

    pulled code related to the API into sub module

---
+++ police-api/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module police.api {
+
+    requires javafx.fxml;
+    requires java.net.http;
+    requires org.slf4j; // was previously slf4j.api;
+    requires com.fasterxml.jackson.annotation; // for model
+    requires com.fasterxml.jackson.databind; // services
+
+    opens com.singingbush.ukcrime.model to javafx.fxml, com.fasterxml.jackson.databind;
+
+    exports com.singingbush.ukcrime.services;
+    exports com.singingbush.ukcrime.model;
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,17 +1,12 @@
 module ukcrime {
     requires javafx.controls;
     requires javafx.fxml;
-
-    requires java.net.http;
-
+    requires org.slf4j; // was previously slf4j.api;
     requires org.apache.logging.log4j;
-    requires com.fasterxml.jackson.databind; // services
-    requires com.fasterxml.jackson.annotation; // for model
 
-    requires slf4j.api;
+    requires police.api;
 
     opens com.singingbush.ukcrime.controllers to javafx.fxml;
-    opens com.singingbush.ukcrime.model to javafx.fxml, com.fasterxml.jackson.databind;
 
     exports com.singingbush.ukcrime;
 }
